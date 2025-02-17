57fc842ab319034e408669f166a05addc6e76d01
faenuccio <filippo.nuccio@univ-st-etienne.fr>
2025-02-17 15:36:21 +0100

    following instructions doc4

--- lakefile.lean
+++ lakefile.lean
@@ -11,9 +11,9 @@ package "GradCourse2425" where
 
 require "leanprover-community" / "mathlib" @ git "bf1047068974f5ebc957a30d55f6ff5d3989f0d3"
 
-meta if get_config? env = some "dev" then -- dev is so not everyone has to build it
-require «doc-gen4» from git
-  "https://github.com/leanprover/doc-gen4" @ "d684d596bcf8fbb114320776b80a1dbfce0a0786"
+-- meta if get_config? env = some "dev" then -- dev is so not everyone has to build it
+-- require «doc-gen4» from git
+--   "https://github.com/leanprover/doc-gen4" @ "d684d596bcf8fbb114320776b80a1dbfce0a0786"
 
 @[default_target]
 lean_lib «GradCourse25» where
